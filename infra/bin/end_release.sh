66dc2484a99a2e1fa3a7f8fe32ce9fbd0a3823fe
raj-pb <prithvi.botcha@murine.org>
2023-02-15 17:17:52 -0800

    update end release

--- infra/bin/end_release.sh
+++ infra/bin/end_release.sh
@@ -30,6 +30,7 @@ find "$root_dir" -type d -mindepth 1 -maxdepth 5 | while read -r project_dir; do
   if [ -f "$project_dir/manifest.yaml" ]; then
     project_name=$(yq eval '.PROJECT_NAME' "$project_dir/manifest.yaml")
     project_branch=$(git branch -r | grep "$project_name/release/" | sed 's/^..//' | sort -V | tail -n1)
+    project_version=$(echo "$project_branch" | awk -F "/" '{print $NF}')
 
     # Check if the last released branch is ahead of develop
     commits_ahead=$(git rev-list --count "develop..$project_branch")
@@ -40,10 +41,22 @@ find "$root_dir" -type d -mindepth 1 -maxdepth 5 | while read -r project_dir; do
 
     # Merge the branch into develop
     if [[ -z "$DRY_RUN" ]]; then
-      git checkout develop && git pull
-      git merge --no-ff "$project_branch"
-      git push
-      echo "$project_name: $project_branch merged to develop."
+      git checkout develop -q && git pull -q
+      if ! git merge --no-ff --no-commit "$project_branch"; then
+        echo "Release aborted. Merge conflicts and retry:"
+        # Continue with the merge and commit the changes
+        git status
+        git merge --abort
+        exit 1
+      fi
+      git commit -m "[release] $project_name:$project_version merge to develop"
+      exit 1
+      new_tag="tags/$project_name/release/$project_version"
+      git tag "$new_tag"
+      git push -u develop "$new_tag"
+      # Create the release notes using gh
+      gh release create "$new_tag" --title "Release version $project_version" --notes "These are the release notes for version $VERSION"
+      echo "$project_name: $project_version merged to develop."
     else
       echo "Dry run: found $project_branch to be merged."
     fi
