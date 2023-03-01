ce059cfd602a1561bb99e9b6001de2a9d12d9b08
raj-pb <prithvi.botcha@murine.org>
2023-02-28 21:53:31 -0800

    fox

--- infra/bin/release_candidate_tags.py
+++ infra/bin/release_candidate_tags.py
@@ -3,23 +3,16 @@
 import argparse
 import semver
 from functools import cmp_to_key
-
-try:
-    from git import Repo, Commit
-except ImportError:
-    print("Error: gitpython not found.")
-    print("run `pip install gitpython`")
-    exit(1)
+from git import Repo, Commit
 
 
 help_text = """
 Bumps release-candidate tags while the project is in the release-flow for the release branches.
-(Typical used by CircleCI while in staging lifecycle after a successful test suite)
+(Used before deploying a release branch while in staging lifecycle after a successful test suite run)
 
 Usage:
-    python3 infrastructure/make-utils/bin/release_candidate_tags.py \
-        -b "origin/gateway/release/0.5.2" \
-        -d gateway
+    python3 ../infrastructure/../release_candidate_tags.py \
+        -b "origin/gateway/release/0.5.2" 
 
     Outputs:
     > branch: origin/gateway/release/0.5.2
@@ -32,13 +25,15 @@ Usage:
 def main(branch: str):
     repo = Repo(".", search_parent_directories=True)
     print(f"branch: {branch}")
-    tags = repo.git.tag("--merged", f"origin/{branch}")
-    tags = [tag for tag in tags.split("\n") if tag.startswith(f"tags/{branch}")]
-    if len(tags) == 0:
+    tags = repo.git.ls_remote("--tags", f"origin")
+    tag_names = [tag.split("refs/tags/")[1] for tag in tags.split("\n")]
+    tag_names = [tag for tag in tag_names if tag.startswith(f"tags/{branch}" and semver.VersionInfo.isvalid(tag.split('/')[-1]))]
+    print (tag_names)
+    if len(tag_names) == 0:
         print(f"No tags found corresponding to the branch {branch}.\n"
-              f"Release branches should be of the form `<project>/release/<SemVer>`")
-        return "branch"
-    latest_tag = max(tags, default=None, key=cmp_to_key(tag_comparison))
+              f"Release branches should be of the form `<project>/release/<semver>`")
+        return 0
+    latest_tag = max(tag_names, default=None, key=cmp_to_key(tag_comparison))
     print(f"latest_tag: {latest_tag}")
     new_version = semver.bump_prerelease(latest_tag.split('/')[-1])
     new_tag = "/".join(latest_tag.split('/')[:-1]+[new_version])
